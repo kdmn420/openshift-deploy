09a6eebce495eabb2cf0dd6b7828aa015101ac70
kdmn420 <kdmn420@gmail.com>
2026-09-21 16:57:54 +0530

    chnages in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
 
     // Fetch tasks from backend
     useEffect(() => {
-        fetchTasks();
+	        fetchTasks();
     }, []);
 
     const fetchTasks = async () => {
@@ -58,7 +58,7 @@ function App() {
     return (
         <div className="App">
             <div className="container">
-                <h1>📝 Task Manager</h1>
+                <h1>📝 My Kubernetes Task Manager</h1>
 
                 <div className="input-section">
                     <input
